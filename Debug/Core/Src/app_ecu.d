ecd0c6cfc8645b5831019b244b9eec318dcd0999
Phan Thanh Dat <phanthanhdatjmg41@gmail.com>
2026-05-24 13:47:38 +0700

    Remove build output files

--- Debug/Core/Src/app_ecu.d
+++
@@ -1,2 +0,0 @@
-Core/Src/app_ecu.o: ../Core/Src/app_ecu.c ../Core/Inc/app_ecu.h
-../Core/Inc/app_ecu.h:
--- Debug/Core/Src/app_log.d
+++
@@ -1,5 +0,0 @@
-Core/Src/app_log.o: ../Core/Src/app_log.c ../Core/Inc/app_log.h \
- ../Core/Inc/app_config.h ../Core/Inc/app_uart.h
-../Core/Inc/app_log.h:
-../Core/Inc/app_config.h:
-../Core/Inc/app_uart.h:
